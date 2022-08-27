93a5b6ff5e7275be0511aed0785da6df0c7b1045
GitHub Action <action@github.com>
2022-08-27 14:09:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/maxzbs/redthemesidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/maxzbs/redthemesidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.maxzbs.redthemesidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.maxzbs.redthemesidea.MyBundle
 
 class MyApplicationService {
 
